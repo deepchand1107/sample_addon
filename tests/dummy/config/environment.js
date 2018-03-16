e188b67ce15df4fd0a563c74f2c8db54b1c65170
Deepchand Pati <deepchand77@gmail.com>
2018-03-16 01:00:00 +0000

    added rootURL in dummy env

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -49,6 +49,7 @@ module.exports = function(environment) {
   }
   if (environment === 'production') {
     ENV.locationType = 'hash';
+    ENV.rootURL = '/sample-addon/';
     ENV.baseUrl = '/sample-addon/';
 
   }
